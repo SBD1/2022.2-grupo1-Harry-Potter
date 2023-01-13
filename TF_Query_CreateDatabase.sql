a44c48b1d6f7c2c0209d562b74d50f8a40aa71b1
leticiaarj <leticiards.04@gmail.com>
2023-01-13 13:28:21 -0300

    Create TF_Query_CreateDatabase.sql

---
+++ TF_Query_CreateDatabase.sql
@@ -0,0 +1,9 @@
+-- -----------------------------------------------------------------
+-- Data Criacao ...........: 13/01/2023
+-- Autor(es) ..............: Letícia Araújo
+-- Versao ..............: 1.0
+-- Banco de Dados .........: PostgreSQL
+-- Descricao .........: Criando Banco de Dados
+-- ------------------------------------------------------------------
+
+CREATE DATABASE 2-2022_grupo1_harrypotter;
