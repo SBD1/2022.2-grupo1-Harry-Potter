43b4bbc6c63e06bbcb218a07ed828ef9b0870d77
André C <andre.correa.silva203@gmail.com>
2023-01-14 18:49:50 -0300

    avanço no jogo
    Co-authored-by: Felipe Candido de Moura <felipecdmoura@users.noreply.github.com>

--- app/database.py
+++ app/database.py
@@ -1,9 +1,51 @@
 import psycopg2
+from area import Area
 
-def create_connection():
-    connect = psycopg2.connect(
-        host="localhost",
-        database="HARRY_POTTER",
-        user="postgres",
-        password="1234")
-    return connect
+
+class DataBase():
+
+    def create_connection():
+        connect = psycopg2.connect(
+            host="localhost",
+            database="HARRY_POTTER",
+            user="postgres",
+            password="Andre203")
+        return connect
+
+    def create_new_character(connection, nome, casa):
+        cursor = connection.cursor()
+
+        querry = "INSERT INTO JOGADOR (idGrimorio, nome, idArea, pontosVida, idCasa) VALUES ( 1 , '%s', 4, 20, %s)" % (
+            nome, casa)
+
+        cursor.execute(querry)
+
+        connection.commit()
+        cursor.close()
+
+    def find_character(connection, name):
+
+        cursor = connection.cursor()
+
+        querry = """SELECT idJogador FROM JOGADOR
+                    WHERE( JOGADOR.nome = '%s') 
+                    """ % (name)
+        cursor.execute(querry)
+        id_player = cursor.fetchone()
+        if(id_player == None):
+            id_player = -1
+        else:
+            id_player = id_player[0]
+
+        cursor.close()
+        return id_player
+
+    def get_area(connection, id):
+        cursor = connection.cursor()
+
+        query = """SELECT * FROM AREA WHERE (AREA.idArea = %s) """ % (id)
+
+        cursor.execute(query)
+        idArea, idRegiao, areaLeste, areaOeste, areaSul, areaNorte = cursor.fetchone()
+
+        return Area(idArea, idRegiao, areaLeste, areaOeste, areaSul, areaNorte)
