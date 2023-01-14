1f90c428cc3501ace0d918c5547c616951c60094
leticiaarj <leticiards.04@gmail.com>
2023-01-14 20:50:13 -0300

    Populando as outras tabelas

--- sql/TF_Query_Popula.sql
+++ sql/TF_Query_Popula.sql
@@ -18,7 +18,7 @@ INSERT INTO MAPA (descricao) VALUES
 INSERT INTO REGIAO (idMapa, descricao, nome) VALUES
 (1, 'portao da escola de magia e bruxaria da regiao', 'Portao da escola'),
 (2, 'Estacao de trem secreta que leva para a escola de bruxaria de Hogwarts', 'Estacao 9 3/4')
-(3, 'r pub e hospedaria bruxo localizado em Londres', 'Calderão Furado'),
+(3, 'Um pub e hospedaria bruxo localizado em Londres', 'Calderão Furado'),
 (4, 'Casa da família Weasley. Localizada nos arredores de Ottery St. Catchpole em Devon, Inglaterra, é uma habitação completamente mágico', 'A Toca'),
 (5, 'Rua dos Alfeneiros, nº 4', 'Casa dos Dursley'),
 (6, 'É uma loja bem popular entre os alunos de Hogwarts pelos variados doces que nela se encontra, vai dos mais simples até os bem exóticos', 'Dedos de Mel');
@@ -69,19 +69,32 @@ INSERT INTO ITEM(nome, acao, valor, tipo, descricaoItem) VALUES
 ('Desiluminador', 'Remove e devolve fontes de luz à suas devidas fontes', 100, 'DEFESA', 'Aparenta ser um isqueiro de prata padrão'),
 ('Livros de Feitiços', 'Explica ao leitor como realizar feitiços', 1000, 'ATAQUE', 'Existem vários tipos'),
 ('Livros de Poções', 'Explica ao leitor como realizar poções', 700, 'DEFESA', 'Existem vários tipos'),
-('Livros de Criaturas Mágicas', 'EXplica sobre as criaturas mágicas', 'ATAQUE', 'Existem vários tipos');
+('Livros de Criaturas Mágicas', 'Explica sobre as criaturas mágicas', 'ATAQUE', 'Existem vários tipos'),
+('Vassoura', 'Usada para moder voar', 500, 'Um dos meios empregados por bruxos para se transportar entre locais'),
+('Vira-tempo', 'Dispositivo utilizado para viajar no tempo', 500, 'O número de vezes que alguém vira a ampulheta corresponde ao número de horas que o indivíduo irá retornar ao passado. No entanto, eles só podem permanecer no passado por cerca de cinco horas');
 
 -- Popula NPC
 INSERT INTO NPC(item, nome) VALUES
-(5, 'Albus Dumbledore'),
-(2, 'Remo Lupin'),
-(3, 'Tiago Potter'),
-(4, 'Ron Weasley'),
-(5, 'Minerva McGonagall'),
+(5, 'Albus Dumbledore'), 
+(2, 'Remo Lupin'), 
+(3, 'Tiago Potter'), 
+(4, 'Ron Weasley'), 
+(5, 'Minerva McGonagall'), 
 (6, 'Severus Snape'),
-(1, 'Rolanda Hooch'),
-(7, 'Rúbeo Hagrid'),
-(5, 'Séptima Vector');
+(1, 'Rolanda Hooch'), 
+(7, 'Rúbeo Hagrid'), 
+(5, 'Séptima Vector'), 
+(5, 'Herpo, o Sujo'),
+(5, 'Morgana le Fay'),
+(5, 'Gerardo Grindelwald'),
+(1, 'Tom Riddle'),
+(1, 'Emerico, o Mal'),
+(1, 'Florean Fortescue'),
+(1, 'Garrick Olivaras'),
+(1, 'Fred Weasley'),
+(1, 'Daisy Dodderidge'),
+(1, 'Sr. Borgin'),
+(1, ' Barnabas Cuffe');
 
 -- Popula DISCIPLINA
 INSERT INTO NPC(NPC, nomeDisciplina, feitico) VALUES
@@ -103,7 +116,7 @@ INSERT INTO NPC(idNPC, casa, disciplina) VALUES
 (9, 4, 6);
 
 -- Popula INSTANCIA_ITEM
-INSERT INTO INSTANCIA_ITEM(idItem)
+INSERT INTO INSTANCIA_ITEM(idItem) VALUES
 (1),
 (2),
 (3),
@@ -112,7 +125,7 @@ INSERT INTO INSTANCIA_ITEM(idItem)
 (6);
 
 -- Popula INVENTARIO
-INSERT INTO INVENTARIO(idJogador, instanciaItem, dinheiro)
+INSERT INTO INVENTARIO(idJogador, instanciaItem, dinheiro) VALUES
 (1, 1, 2000),
 (2, 2, 2000),
 (3, 3, 2000),
@@ -120,6 +133,101 @@ INSERT INTO INVENTARIO(idJogador, instanciaItem, dinheiro)
 (5, 5, 200),
 (6, 6, 2000);
 
+-- Popula INSTANCIA_JOGADOR_DISCIPLINA
+INSERT INTO INSTANCIA_JOGADOR_DISCIPLINA(idJogador, idDisciplina) VALUES
+(1, 2),
+(2, 3),
+(3, 4),
+(4, 5),
+(5, 6),
+(6, 1);
+
+-- Popula HABILIDADE
+INSERT INTO HABILIDADE(nomeHabilidade, dano, descricao) VALUES
+('Vidência', 3, 'Capacidade de saber os eventos futuros'),
+('Animagos', 5, 'Capacidade em se transformar em animais'),
+('Metamorfomagos', 0, 'Muda a forma de seu nariz, da cor de seu cabelo e outros atributos físicos'),
+('Legilimência', 7, 'Capacidade de entrar nas mentes das outras pessoas'),
+('Oclumência', 0, 'Capacidade de proteger suas mentes dos Legilimens'),
+('Ofidioglossia', 5, 'Capacidade natural de falar língua de cobra');
+('Realizar Feitiços', 5, 'Realizar feitiços com a varinha e com ou sem auxílio de livros');
+
+-- Popula INIMIGO
+INSERT INTO INIMIGO(idNPC, idHabilidade, moedas) VALUES
+(9, 6, 1000),
+(10, 6, 1000),
+(11, 6, 1000),
+(12, 6, 1000),
+(13, 6, 1000),
+(14, 6, 1000);
+
+-- Popula INSTANCIA_INIMIGO
+INSERT INTO INSTANCIA_INIMIGO(idNPC, idArea, idInstanciaItem, pontosVida, multiplicador) VALUES
+(9, 1, 400, 1),
+(10, 2, 400, 2),
+(11, 3, 400, 3),
+(12, 4, 400, 1),
+(13, 5, 400, 2),
+(14, 6, 400, 3);
+
+-- Popula FERRAMENTA
+INSERT INTO FERRAMENTA(idItem, forca) VALUES
+(1, 1000),
+(2, 0),
+(3, 500),
+(4, 0),
+(8), 500,
+(9, 500);
+
+-- Popula POCAO
+INSERT INTO POCAO(idItem, ingrediente) VALUES
+(6, 'Sanguinária'),
+(6, 'Osso'),
+(6, 'Diafanina'),
+(6, 'Bezoar'),
+(6, 'Ovo de Cinzal'),
+(6, 'Chifre de Unicórnio');
+
+-- Popula LOJA
+INSERT INTO LOJA(idNPC, idArea, idInstanciaItem, descricao) VALUES
+(15, 3, 1, ' Sorveteria Florean Fortescue'),
+(16, 3, 1, 'Olivaras Varinhas'),
+(17, 3, 1, 'Gemialidades Weasley'),
+(18, 3, 1, ' Caldeirão Furado'),
+(19, 3, 1, ' Borgin e Burkes'),
+(20, 3, 1, 'Profeta Diário');
+
+-- Popula LIVRO
+INSERT INTO LIVRO(idItem, feitico) VALUES
+(5, 1),
+(5, 2),
+(5, 3),
+(5, 4),
+(5, 5),
+(5, 6);
+
+-- Popula Instancia_NPC_Tipo
+INSERT INTO INSTANCIA_NPC_TIPO(idNPC, idArea, tipo) VALUES
+(1, 1, 'Professor'),
+(2, 1, 'Professor'),
+(3, 1, 'Amigo'),
+(4, 1, 'Amigo'),
+(5, 1, 'Professor'),
+(6, 1, 'Professor'),
+(7, 1, 'Professor'),
+(8, 1, 'Professor'),
+(9, 1, 'Professor'),
+(10, 1, 'Inimigo'),
+(11, 1, 'Inimigo'),
+(12, 2, 'Inimigo'),
+(13, 3, 'Inimigo'),
+(14, 4, 'Inimigo'),
+(15, 2, 'Inimigo'),
+(16, 3, 'Vendedor'),
+(17, 3, 'Vendedor'),
+(18, 3'Vendedor'),
+(19, 3, 'Vendedor'),
+(20, 3, 'Vendedor');
 
 
 
