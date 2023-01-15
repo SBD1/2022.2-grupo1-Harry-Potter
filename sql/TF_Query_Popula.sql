1d07b1b8df329edbee38b186981d85fe17893c62
leticiaarj <leticiards.04@gmail.com>
2023-01-14 21:27:55 -0300

    Update TF_Query_Popula.sql

--- sql/TF_Query_Popula.sql
+++ sql/TF_Query_Popula.sql
@@ -1,5 +1,17 @@
+-- -----------------------------------------------------------------
+-- Data Criacao ...........: 13/01/2023
+-- Autor(es) ..............: André
+-- Versao ..............: 1.0
+-- Banco de Dados .........: PostgreSQL
+-- Descricao .........: Iniciando popula das tabelas
+-- ------------------------------------------------------------------
+-- Data Atualizacao ...........: 14/01/2023
+-- Autor(es) ..............: Letícia Araújo
+-- Descricao .........: Finalizando popula das tabelas.
+-- ------------------------------------------------------------------
+
 -- Popula casas
-INSERT INTO casa (nomecasa, petcasa ) VALUES
+INSERT INTO CASA (nomecasa, petcasa ) VALUES
 ('Grifinória', 'Águia'),
 ('Sonserina', 'Cobra'),
 ('Corvinal', 'Corvo'),
@@ -17,20 +29,20 @@ INSERT INTO MAPA (descricao) VALUES
 -- Popula Regiao
 INSERT INTO REGIAO (idMapa, descricao, nome) VALUES
 (1, 'portao da escola de magia e bruxaria da regiao', 'Portao da escola'),
-(2, 'Estacao de trem secreta que leva para a escola de bruxaria de Hogwarts', 'Estacao 9 3/4')
+(2, 'Estacao de trem secreta que leva para a escola de bruxaria de Hogwarts', 'Estacao 9 3/4'),
 (3, 'Um pub e hospedaria bruxo localizado em Londres', 'Calderão Furado'),
 (4, 'Casa da família Weasley. Localizada nos arredores de Ottery St. Catchpole em Devon, Inglaterra, é uma habitação completamente mágico', 'A Toca'),
 (5, 'Rua dos Alfeneiros, nº 4', 'Casa dos Dursley'),
 (6, 'É uma loja bem popular entre os alunos de Hogwarts pelos variados doces que nela se encontra, vai dos mais simples até os bem exóticos', 'Dedos de Mel');
 
 -- Popula Area
-INSERT INTO AREA(idRegiao, areaLeste, areaOeste, areaSul, areaNorte) VALUES
-(1, NULL, 3, NULL, 2),
-(2, NULL, NULL, 1, NULL),
-(3, 1, NULL, 4, NULL),
-(4, 1, NULL, 4, NULL),
-(5, 1, NULL, 4, NULL),
-(6, NULL, NULL, NULL, 3);
+INSERT INTO AREA(idArea, idRegiao, areaLeste, areaOeste, areaSul, areaNorte) VALUES
+(1, 1, 1, 1, 1, 1),
+(2, 2, 2, 2, 2, 2),
+(3, 3, 1, 1, 4, 1),
+(4, 4, 1, 1, 4, 1),
+(5, 5, 1, 4, 4, 4),
+(6, 6, 1, 1, 1, 3);
 
 -- Popula Feitico
 INSERT INTO FEITICO (nome, efeito, ponto, quantidadeUso) VALUES
@@ -54,24 +66,24 @@ INSERT INTO GRIMORIO (numSlots, feitico) VALUES
 
 -- Popula Jogador
 INSERT INTO JOGADOR (idGrimorio, nome, idArea, pontosVida, idCasa) VALUES
-(1, 'Harry Potter', 4, 20, 1)
-(2, 'Lily Williamson', 1, 20, 1)
-(3, 'Ben Kent', 2, 20, 2)
-(4, 'Morgan Kerr', 3, 20, 3)
-(5, 'Edward Simpson', 4, 20, 4)
+(1, 'Harry Potter', 4, 20, 1),
+(2, 'Lily Williamson', 1, 20, 1),
+(3, 'Ben Kent', 2, 20, 2),
+(4, 'Morgan Kerr', 3, 20, 3),
+(5, 'Edward Simpson', 4, 20, 4),
 (6, 'Emma Parkes', 5, 20, 1);
 
 -- Popula Item
 INSERT INTO ITEM(nome, acao, valor, tipo, descricaoItem) VALUES
-('Varinha', 'Realiza Feiticos', 900, 'ATAQUE', 'Uma varinha é um instrumento mágico quase consciente pelo qual um bruxo canaliza seus poderes mágicos'),
-('Mapa do Maroto', 'Mostra a localização exata de cada pessoa dentro dos limites de Hogwarts', 400, 'LOCOMOCAO', 'Indica caminhos, passagens secretas e outros pontos de interesse pelo castelo'),
-('Capa da Invisibilidade', 'Torna o usuário invisível', 600, 'DEFESA', 'Única capa de invisibilidade conhecida que não se desvaneceria com a idade e proporcionaria proteção eterna ao usuário'),
-('Desiluminador', 'Remove e devolve fontes de luz à suas devidas fontes', 100, 'DEFESA', 'Aparenta ser um isqueiro de prata padrão'),
-('Livros de Feitiços', 'Explica ao leitor como realizar feitiços', 1000, 'ATAQUE', 'Existem vários tipos'),
-('Livros de Poções', 'Explica ao leitor como realizar poções', 700, 'DEFESA', 'Existem vários tipos'),
-('Livros de Criaturas Mágicas', 'Explica sobre as criaturas mágicas', 'ATAQUE', 'Existem vários tipos'),
-('Vassoura', 'Usada para moder voar', 500, 'Um dos meios empregados por bruxos para se transportar entre locais'),
-('Vira-tempo', 'Dispositivo utilizado para viajar no tempo', 500, 'O número de vezes que alguém vira a ampulheta corresponde ao número de horas que o indivíduo irá retornar ao passado. No entanto, eles só podem permanecer no passado por cerca de cinco horas');
+('Varinha', 'Realiza Feiticos', 90, 'ATAQUE', 'Onde um bruxo canaliza seus poderes mágicos'),
+('Mapa do Maroto', 'Mostra a localização exata de cada pessoa dentro dos limites de Hogwarts', 40, 'LOCOMOCAO', 'Indica pontos de interesse pelo castelo'),
+('Capa da Invisibilidade', 'Torna o usuário invisível', 60, 'DEFESA', 'Única capa de invisibilidade conhecida'),
+('Desiluminador', 'Remove e devolve fontes de luz à suas devidas fontes', 10, 'DEFESA', 'Aparenta ser um isqueiro de prata padrão'),
+('Livros de Feitiços', 'Explica ao leitor como realizar feitiços', 50, 'ATAQUE', 'Existem vários tipos'),
+('Livros de Poções', 'Explica ao leitor como realizar poções', 70, 'DEFESA', 'Existem vários tipos'),
+('Livros de Criaturas Mágicas', 'Explica sobre as criaturas mágicas', 50, 'ATAQUE', 'Existem vários tipos'),
+('Vassoura', 'Usada para moder voar', 50, 'DEFESA', 'Transporte entre locais'),
+('Vira-tempo', 'Utilizado para viajar no tempo', 50, 'DEFESA', 'Retorna horas ao passado');
 
 -- Popula NPC
 INSERT INTO NPC(item, nome) VALUES
@@ -97,22 +109,22 @@ INSERT INTO NPC(item, nome) VALUES
 (1, ' Barnabas Cuffe');
 
 -- Popula DISCIPLINA
-INSERT INTO NPC(NPC, nomeDisciplina, feitico) VALUES
-(1, 'DEFESA CONTRA AS ARTES DAS TREVAS', 5),
+INSERT INTO DISCIPLINA (NPC, nomeDisciplina, feitico) VALUES
+(1, 'DEFESA ARTE TREVAS', 5),
 (2, 'TRANSFORMAÇÕES', 7),
 (7, 'VÔO COM VASSOURAS', 7),
-(8, 'TRATO DE CRIATURAS MÁGICAS', 2),
+(8, 'TRATO CRIATURAS', 2),
 (6, 'POÇÕES', 2),
-(9, 'ARITMANCIA', 2),
+(9, 'ARITMANCIA', 2);
 
 -- Popula PROFESSOR
-INSERT INTO NPC(idNPC, casa, disciplina) VALUES
+INSERT INTO PROFESSOR(idNPC, casa, disciplina) VALUES
 (1, 1, 1),
 (2, 1, 1),
 (5, 1, 2),
 (6, 2, 5),
 (7, 3, 3),
-(8, 1, 4)
+(8, 1, 4),
 (9, 4, 6);
 
 -- Popula INSTANCIA_ITEM
@@ -146,11 +158,11 @@ INSERT INTO INSTANCIA_JOGADOR_DISCIPLINA(idJogador, idDisciplina) VALUES
 INSERT INTO HABILIDADE(nomeHabilidade, dano, descricao) VALUES
 ('Vidência', 3, 'Capacidade de saber os eventos futuros'),
 ('Animagos', 5, 'Capacidade em se transformar em animais'),
-('Metamorfomagos', 0, 'Muda a forma de seu nariz, da cor de seu cabelo e outros atributos físicos'),
+('Metamorfomagos', 0, 'Muda a forma de seus atributos físicos'),
 ('Legilimência', 7, 'Capacidade de entrar nas mentes das outras pessoas'),
 ('Oclumência', 0, 'Capacidade de proteger suas mentes dos Legilimens'),
-('Ofidioglossia', 5, 'Capacidade natural de falar língua de cobra');
-('Realizar Feitiços', 5, 'Realizar feitiços com a varinha e com ou sem auxílio de livros');
+('Ofidioglossia', 5, 'Capacidade natural de falar língua de cobra'),
+('Realizar Feitiços', 5, 'Realizar feitiços com a varinha');
 
 -- Popula INIMIGO
 INSERT INTO INIMIGO(idNPC, idHabilidade, moedas) VALUES
@@ -163,12 +175,12 @@ INSERT INTO INIMIGO(idNPC, idHabilidade, moedas) VALUES
 
 -- Popula INSTANCIA_INIMIGO
 INSERT INTO INSTANCIA_INIMIGO(idNPC, idArea, idInstanciaItem, pontosVida, multiplicador) VALUES
-(9, 1, 400, 1),
-(10, 2, 400, 2),
-(11, 3, 400, 3),
-(12, 4, 400, 1),
-(13, 5, 400, 2),
-(14, 6, 400, 3);
+(9, 1, 1, 400, 1),
+(10, 2, 2, 400, 2),
+(11, 3, 3, 400, 3),
+(12, 4, 4, 400, 1),
+(13, 5, 5, 400, 2),
+(14, 6, 6, 400, 3);
 
 -- Popula FERRAMENTA
 INSERT INTO FERRAMENTA(idItem, forca) VALUES
@@ -176,7 +188,7 @@ INSERT INTO FERRAMENTA(idItem, forca) VALUES
 (2, 0),
 (3, 500),
 (4, 0),
-(8), 500,
+(8, 500),
 (9, 500);
 
 -- Popula POCAO
@@ -225,9 +237,6 @@ INSERT INTO INSTANCIA_NPC_TIPO(idNPC, idArea, tipo) VALUES
 (15, 2, 'Inimigo'),
 (16, 3, 'Vendedor'),
 (17, 3, 'Vendedor'),
-(18, 3'Vendedor'),
+(18, 3, 'Vendedor'),
 (19, 3, 'Vendedor'),
 (20, 3, 'Vendedor');
-
-
-
