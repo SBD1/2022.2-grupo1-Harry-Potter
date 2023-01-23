64a09dd99eef566496809bda0bde6b78a44a800e
leticiaarj <leticiards.04@gmail.com>
2023-01-23 09:06:16 -0300

    Triggers para especialização e generalização

---
+++ sql/TF_Query_TriggersAndStoredProcedures.sql
@@ -0,0 +1,40 @@
+-- TRIGGER ITEM (Generalização e especialização)
+
+CREATE OR REPLACE FUNCTION check_livro_fc() RETURNS trigger AS $check_livro_fc$
+BEGIN
+	PERFORM * FROM LIVRO WHERE idItem = NEW.idItem;
+	IF FOUND THEN
+	RAISE EXCEPTION 'Este item já existe na tabela de livros';
+	END IF;
+	RETURN NEW; 
+END;
+$check_livro_fc$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_livro
+BEFORE UPDATE OR INSERT ON FERRAMENTA
+FOR EACH ROW EXECUTE PROCEDURE check_livro_fc();
+
+CREATE TRIGGER check_livro
+BEFORE UPDATE OR INSERT ON POCAO
+FOR EACH ROW EXECUTE PROCEDURE check_livro_fc();
+
+DROP TRIGGER check_livro ON FERRAMENTA;
+DROP TRIGGER check_livro ON POCAO;
+
+-- TRIGGER NPC (Generalização e especialização)
+
+CREATE OR REPLACE FUNCTION check_inimigo_fc() RETURNS trigger AS $check_inimigo_fc$
+BEGIN
+	PERFORM * FROM INIMIGO WHERE idNPC = NEW.idNPC;
+	IF FOUND THEN
+	RAISE EXCEPTION 'Este NPC já existe na tabela de inimigos';
+	END IF;
+	RETURN NEW; 
+END;
+$check_inimigo_fc$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_inimigo
+BEFORE UPDATE OR INSERT ON PROFESSOR
+FOR EACH ROW EXECUTE PROCEDURE check_inimigo_fc();
+
+DROP TRIGGER check_inimigo ON PROFESSOR;
